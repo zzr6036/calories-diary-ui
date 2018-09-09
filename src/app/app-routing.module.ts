84908a5fd58a939caf196fcb61fcdb82340ef2d2
Zhiyue Yi <lane0223@hotmail.com>
2018-09-09 22:49:53 +0800

    draft login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'tab', loadChildren: './tabs/tabs.module#TabsPageModule' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+	Component,
+	OnInit
+} from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+	selector: 'app-login',
+	templateUrl: './login.page.html',
+	styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+	registerCredentials = {
+		email: '',
+		password: ''
+	};
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
+	public createAccount() {
+
+	}
+
+	public login() {
+
+	}
+
+	showLoading() {
+
+	}
+
+	showError(text) {
+
+	}
 }
