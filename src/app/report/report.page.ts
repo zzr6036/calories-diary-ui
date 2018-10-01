fc2244cea1a5abd7d125e8c7cb86170d1ae471d4
Zhiyue Yi <lane0223@hotmail.com>
2018-10-01 22:58:42 +0800

    Added report ui structure. Pending for data population

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import Chart from 'chart.js'
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.page.html',
   styleUrls: ['./report.page.scss'],
 })
-export class ReportPage implements OnInit {
+export class ReportPage implements OnInit, AfterViewInit {
+  public lineChart: any;
 
+  @ViewChild('lineCanvas') lineCanvas;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.lineChart = this.getLineChart();
+    }, 350);
+  }
+
+  getChart(context, chartType, data, options?) {
+    return new Chart(context, {
+      data,
+      options,
+      type: chartType,
+    });
+  }
+
+  getLineChart() {
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow'],
+      datasets: [
+        {
+          data: [
+            {
+            x: 10,
+            y: 20
+          }, {
+            x: 15,
+            y: 10
+          }, {
+            x: 20,
+            y: 15
+          }
+        ]
+        }]
+    };
+    console.log(this.lineCanvas.nativeElement);
+    return this.getChart(this.lineCanvas.nativeElement, 'line', data);
   }
 
 }
