97a96ee1a21ec6d503f19179f60c41bc2cf2f384
Ziru <zzrchirstina@gmail.com>
2018-10-31 20:11:11 +0800

    project

--- src/app/global.ts
+++ src/app/global.ts
@@ -2,5 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable() 
 export class Globals {
-    resourceUrl: string = 'http://localhost:5000'
+    // resourceUrl: string = 'http://localhost:5000'
+    resourceUrl: string = 'http://localhost:5000/10.27.242.44'
 }
